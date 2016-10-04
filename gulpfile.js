3aedac380cfed5f52ee309c36d9b59239cc14bac
[John Patterson] <pattersonjc1@gmail.com>
2016-10-03 21:38:47 -0400

    added a bit more Sass

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,15 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
  gulp.watch('./sass/*.scss', ['sass']);
 });
+
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    // var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./src/*.css')
+        // .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        // .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
